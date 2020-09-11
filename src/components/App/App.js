f279351102a34ccba0676a9204bbfe544d46127f
indraantoor <indraantoor@gmail.com>
2020-09-11 17:12:31 +0530

    Optimized Image Sizes and code, Test and working with api key

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,32 +1,32 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-import BusinessList from '../BusinessList/BusinessList';
-import SearchBar from '../SearchBar/SearchBar';
+import BusinessList from "../BusinessList/BusinessList";
+import SearchBar from "../SearchBar/SearchBar";
 
-import Yelp from '../../util/Yelp';
+import Yelp from "../../util/Yelp";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      businesses: []
+      businesses: [],
     };
 
     this.searchYelp = this.searchYelp.bind(this);
   }
 
   searchYelp(term, location, sortBy) {
-    Yelp.search(term, location, sortBy).then(businesses => {
-      this.setState({businesses: businesses});
+    Yelp.search(term, location, sortBy).then((businesses) => {
+      this.setState({ businesses: businesses });
     });
   }
 
   render() {
     return (
       <div className="App">
-        <h1>ravenous</h1>
+        <h1>Find A Place</h1>
         <SearchBar searchYelp={this.searchYelp} />
         <BusinessList businesses={this.state.businesses} />
       </div>
